9f11605d36c8595342e818fe9e866bd945007e5f
fileall <39478417+fileall@users.noreply.github.com>
2023-09-23 10:36:11 +0800

    a

--- library/utils.js
+++ library/utils.js
@@ -11,39 +11,39 @@ import api from './api.js';
 
 
 //  公众号的 jssdk 方法
- export  function	officialAccount(callback){
-			    let path =location.href.split('#')[0]
-			    let appid = $publicConfig.appid;
-			    api.get_jssdk_sign({   // 后端签名
-			    	url:path
-			    }).then(res=>{
-					if(res.code==200){
-						let apiList = [ // 可能需要用到的能力
-						            'updateAppMessageShareData',
-						            'chooseWXPay',
-									'onMenuShareAppMessage',
-									'hideOptionMenu',
-									'openAddress',
-						        ];
-						     //后台请求的 信息
-						        let info = {
-						            debug: false, // 调试，发布的时候改为false
-						            appId: appid,
-						            nonceStr: res.data.noncestr,
-						            timestamp: res.data.timestamp,
-						            signature: res.data.signature,
-						            jsApiList: apiList
-						        };
-								console.log(info);
-						        jweixin.config(info);
-						        jweixin.ready(res => {
-									if (callback) callback(jweixin);  //配置成功  回调
-								 });
-								jweixin.error(err => {
-								    console.log('config fail:', err);
-								});
-					}
-				})
+export function officialAccount(callback) {
+	let path = location.href.split('#')[0]
+	let appid = $publicConfig.appid;
+	api.get_jssdk_sign({ // 后端签名
+		url: path
+	}).then(res => {
+		if (res.code == 200) {
+			let apiList = [ // 可能需要用到的能力
+				'updateAppMessageShareData',
+				'chooseWXPay',
+				'onMenuShareAppMessage',
+				'hideOptionMenu',
+				'openAddress',
+			];
+			//后台请求的 信息
+			let info = {
+				debug: false, // 调试，发布的时候改为false
+				appId: appid,
+				nonceStr: res.data.noncestr,
+				timestamp: res.data.timestamp,
+				signature: res.data.signature,
+				jsApiList: apiList
+			};
+			console.log(info);
+			jweixin.config(info);
+			jweixin.ready(res => {
+				if (callback) callback(jweixin); //配置成功  回调
+			});
+			jweixin.error(err => {
+				console.log('config fail:', err);
+			});
+		}
+	})
 }
 
 export function checkTel(tel) {
@@ -75,117 +75,126 @@ export function toast(title, icon, image, mask) {
 	})
 }
 
-	//图片压缩
-	export function	compressImage(path){
-			return  new Promise((resolve, reject)=>{
-				uni.compressImage({
-				  src: path,
-				  quality: 80,
-				  success: res => {
-					  resolve(res.tempFilePath)
-					 }
-				})
-			})
-		}
+//图片压缩
+export function compressImage(path) {
+	return new Promise((resolve, reject) => {
+		uni.compressImage({
+			src: path,
+			quality: 80,
+			success: res => {
+				resolve(res.tempFilePath)
+			}
+		})
+	})
+}
 
 //  页面跳转
- export function linkTo(name, params) {
+export function linkTo(name, params) {
+
+	if (name.substr(0, 1) == '/') {
+		let url = `..${name}`;
+	} else {
 		let url = `../${name}/${name}`;
-		if (typeof params == 'object') {
-			let str = '?';
-			let paramsArr = [];
-			for (let k in params) {
-				let temp = `${k}=${params[k]}`;
-				paramsArr.push(temp)
-			};
-			
-			str = str + paramsArr.join('&');
-			url = url + str;
-		}
-		uni.reLaunch({
-			url: url
-		})
 	}
-	
-	export function navigateTo(name, params) {
-			let url = `../${name}/${name}`;
-			if (typeof params == 'object') {
-				let str = '?';
-				let paramsArr = [];
-				for (let k in params) {
-					let temp = `${k}=${params[k]}`;
-					paramsArr.push(temp)
-				};
-				
-				str = str + paramsArr.join('&');
-				url = url + str;
-			}
-			uni.navigateTo({
-				url: url
-			})
-		}
-		
-	
-	
+	if (typeof params == 'object') {
+		let str = '?';
+		let paramsArr = [];
+		for (let k in params) {
+			let temp = `${k}=${params[k]}`;
+			paramsArr.push(temp)
+		};
+
+		str = str + paramsArr.join('&');
+		url = url + str;
+	}
+	uni.reLaunch({
+		url: url
+	})
+}
+
+export function navigateTo(name, params) {
+	if (name.substr(0, 1) == '/') {
+		let url = `..${name}`;
+	} else {
+		let url = `../${name}/${name}`;
+	}
+	if (typeof params == 'object') {
+		let str = '?';
+		let paramsArr = [];
+		for (let k in params) {
+			let temp = `${k}=${params[k]}`;
+			paramsArr.push(temp)
+		};
+
+		str = str + paramsArr.join('&');
+		url = url + str;
+	}
+	uni.navigateTo({
+		url: url
+	})
+}
 
 
- //删除数组中指定的元素
-export function  removeByValue (arr, val){
+
+
+
+//删除数组中指定的元素
+export function removeByValue(arr, val) {
 	for (var i = 0; i < arr.length; i++) {
 		if (arr[i] == val) {
 			arr.splice(i, 1);
 			break;
 		}
 	}
-	
+
 }
 
 // 图片转base 64  连个参数1.图片资源路径 2.要赋值的变量
 
 export function urlTobase64(url, name) {
-				var toBase64Url;
-				uni.request({
-					url: url,
-					method: 'GET',
-					responseType: 'arraybuffer',
-					success: async res => {
-						let base64 = wx.arrayBufferToBase64(res.data); //把arraybuffer转成base64
-						toBase64Url = 'data:image/jpeg;base64,' + base64; //不加上这串字符，在页面无法显示
-						this[name] = toBase64Url;
-					}
-				});
+	var toBase64Url;
+	uni.request({
+		url: url,
+		method: 'GET',
+		responseType: 'arraybuffer',
+		success: async res => {
+			let base64 = wx.arrayBufferToBase64(res.data); //把arraybuffer转成base64
+			toBase64Url = 'data:image/jpeg;base64,' + base64; //不加上这串字符，在页面无法显示
+			this[name] = toBase64Url;
+		}
+	});
 }
 
 
 //吊起支付
-export function	pay(type,orderInfo){
-		let provider;
-		if(type == 1){
-			//支付宝
-			provider = "alipay"
-		}else{
-			//微信
-			provider = "wxpay"
-		}
-		return new Promise((resolve,reject)=>{
-			uni.requestPayment({
-			    provider: provider,
-			    orderInfo: orderInfo, //微信、支付宝订单数据
-			    success: function (res) {
-			        resolve({
-						result:true,
-						data:res
-					})
-			    },
-			    fail: function (err) {
-			        resolve({
-			        	result:false,
-			        	data:err
-			        })
-			    }
-			});
-		})
+export function pay(type, orderInfo) {
+	let provider;
+	if (type == 1) {
+		//支付宝
+		provider = "alipay"
+	} else {
+		//微信
+		provider = "wxpay"
 	}
+	return new Promise((resolve, reject) => {
+		uni.requestPayment({
+			provider: provider,
+			orderInfo: orderInfo, //微信、支付宝订单数据
+			success: function(res) {
+				resolve({
+					result: true,
+					data: res
+				})
+			},
+			fail: function(err) {
+				resolve({
+					result: false,
+					data: err
+				})
+			}
+		});
+	})
+}
 
 
 export function hideToast() {
@@ -199,20 +208,20 @@ export function hideLoading() {
 }
 
 //显示加载中
-export function	showLoading(title = "加载中") {
-		return new Promise((resolve, reject) => {
-			uni.showLoading({
-				title: title,
-				mask: true,
-				success: () => {
-					resolve(true);
-				},
-				fail: () => {
-					resolve(false);
-				}
-			});
-		})
-	}
+export function showLoading(title = "加载中") {
+	return new Promise((resolve, reject) => {
+		uni.showLoading({
+			title: title,
+			mask: true,
+			success: () => {
+				resolve(true);
+			},
+			fail: () => {
+				resolve(false);
+			}
+		});
+	})
+}
 // 弹出提示
 export function modal(title, content, showCancel, cancelText, confirmText, cancelColor, confirmColor) {
 	return new Promise((resolve, reject) => {
@@ -299,7 +308,7 @@ export function checkID(id) {
 }
 
 // 生成随机数
- function getRandom(n, m) {
+function getRandom(n, m) {
 	n = Number(n)
 	m = Number(m)
 	if (n > m) {
@@ -313,32 +322,33 @@ export function checkID(id) {
 //根据经纬度计算距离
 //开始地址 纬度 经度   结束地址 纬度 经度
 export function getDistance($lat1, $lng1, $lat2, $lng2) {
-		 let	$earthRadius = 6367000;   //地球半径 单位m
-		 $lat1 = ($lat1 * Math.PI) / 180;
-		 $lng1 = ($lng1 * Math.PI) / 180;
-		 $lat2 = ($lat2 * Math.PI) / 180;
-		 $lng2 = ($lng2 * Math.PI) / 180;
-		let	$calcLongitude = $lng2 - $lng1;
-		let	$calcLatitude = $lat2 - $lat1;
-		let	$stepOne = Math.pow(Math.sin($calcLatitude / 2), 2) + Math.cos($lat1) * Math.cos($lat2) * Math.pow(Math.sin($calcLongitude / 2), 2);
-		let	$stepTwo = 2 * Math.asin(Math.min(1, Math.sqrt($stepOne)));
-		let	$calculatedDistance = $earthRadius * $stepTwo;
-		return Math.round($calculatedDistance);
+	let $earthRadius = 6367000; //地球半径 单位m
+	$lat1 = ($lat1 * Math.PI) / 180;
+	$lng1 = ($lng1 * Math.PI) / 180;
+	$lat2 = ($lat2 * Math.PI) / 180;
+	$lng2 = ($lng2 * Math.PI) / 180;
+	let $calcLongitude = $lng2 - $lng1;
+	let $calcLatitude = $lat2 - $lat1;
+	let $stepOne = Math.pow(Math.sin($calcLatitude / 2), 2) + Math.cos($lat1) * Math.cos($lat2) * Math.pow(Math.sin(
+		$calcLongitude / 2), 2);
+	let $stepTwo = 2 * Math.asin(Math.min(1, Math.sqrt($stepOne)));
+	let $calculatedDistance = $earthRadius * $stepTwo;
+	return Math.round($calculatedDistance);
 }
 // 选择系统相册
 
-  export function chooseImg(count = 1) {
-		return new Promise((resolve, reject) => {
-			  uni.chooseImage({
-					count: count,
-					sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
-					sourceType: ['album'], //从相册选择
-					success: function(res) {
-						resolve(res);
-					}
-				});
-		})
-	}
+export function chooseImg(count = 1) {
+	return new Promise((resolve, reject) => {
+		uni.chooseImage({
+			count: count,
+			sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
+			sourceType: ['album'], //从相册选择
+			success: function(res) {
+				resolve(res);
+			}
+		});
+	})
+}
 // 生成图形随机验证码
 export function getRandomCode() {
 	var codeStr =
@@ -379,39 +389,39 @@ export function formatDate(date, fmt) {
 
 // 格式化时间  将秒格式成 00:00:00:00
 export function formatTime(seconds) {
-			// 小于等于0的话，结束倒计时
-			seconds <= 0 && this.end();
-			let [day, hour, minute, second] = [0, 0, 0, 0];
-			day = Math.floor(seconds / (60 * 60 * 24));
-			// 判断是否显示“天”参数，如果不显示，将天部分的值，加入到小时中
-			// hour为给后面计算秒和分等用的(基于显示天的前提下计算)
-			hour = Math.floor(seconds / (60 * 60)) - day * 24;
-			// showHour为需要显示的小时
-			let showHour = null;
-			if(this.showDays) {
-				showHour = hour;
-			} else {
-				// 如果不显示天数，将“天”部分的时间折算到小时中去
-				showHour = Math.floor(seconds / (60 * 60));
-			}
-			minute = Math.floor(seconds / 60) - hour * 60 - day * 24 * 60;
-			second = Math.floor(seconds) - day * 24 * 60 * 60 - hour * 60 * 60 - minute * 60;
-			// 如果小于10，在前面补上一个"0"
-			
-			showHour = showHour < 10 ? '0' + showHour : showHour;
-			
-			minute = minute < 10 ? '0' + minute : minute;
-			second = second < 10 ? '0' + second : second;
-			
-			let obj ={
-				"day":day,
-				"hour":showHour,
-				"minute":minute,
-				"second":second
-			}
-			return obj
-			// this.d = day;
-			// this.h = showHour;
-			// this.i = minute;
-			// this.s = second;
+	// 小于等于0的话，结束倒计时
+	seconds <= 0 && this.end();
+	let [day, hour, minute, second] = [0, 0, 0, 0];
+	day = Math.floor(seconds / (60 * 60 * 24));
+	// 判断是否显示“天”参数，如果不显示，将天部分的值，加入到小时中
+	// hour为给后面计算秒和分等用的(基于显示天的前提下计算)
+	hour = Math.floor(seconds / (60 * 60)) - day * 24;
+	// showHour为需要显示的小时
+	let showHour = null;
+	if (this.showDays) {
+		showHour = hour;
+	} else {
+		// 如果不显示天数，将“天”部分的时间折算到小时中去
+		showHour = Math.floor(seconds / (60 * 60));
+	}
+	minute = Math.floor(seconds / 60) - hour * 60 - day * 24 * 60;
+	second = Math.floor(seconds) - day * 24 * 60 * 60 - hour * 60 * 60 - minute * 60;
+	// 如果小于10，在前面补上一个"0"
+
+	showHour = showHour < 10 ? '0' + showHour : showHour;
+
+	minute = minute < 10 ? '0' + minute : minute;
+	second = second < 10 ? '0' + second : second;
+
+	let obj = {
+		"day": day,
+		"hour": showHour,
+		"minute": minute,
+		"second": second
+	}
+	return obj
+	// this.d = day;
+	// this.h = showHour;
+	// this.i = minute;
+	// this.s = second;
 }
